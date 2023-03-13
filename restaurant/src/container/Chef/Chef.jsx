3bc87336d3bb067ce1c369b413e46739a1475b11
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:19:52 +0300

    creating the quote section for owner

--- restaurant/src/container/Chef/Chef.jsx
+++ restaurant/src/container/Chef/Chef.jsx
@@ -13,7 +13,10 @@ const Chef = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chef's word" />
       <h1 className="headtext__cormorant">What we believe in</h1>
-  </div>
-);
-
-export default Chef;
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_image" />
+          <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .</p>
+        </div>
+        <p className="p__opensans"> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
+      </div>

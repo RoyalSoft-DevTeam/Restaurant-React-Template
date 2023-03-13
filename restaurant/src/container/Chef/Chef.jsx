7ec2eeaa68da30c12c4a85c25b7a437c6d3530ce
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:19:01 +0300

    creating the image wrapper

--- restaurant/src/container/Chef/Chef.jsx
+++ restaurant/src/container/Chef/Chef.jsx
@@ -6,8 +6,13 @@ import './Chef.css';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
   </div>
 );
 

3d0b9d94e31f61da6c32e5a4eca0135d277c21fb
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:58:16 +0300

    created the rest of the section

--- restaurant/src/container/Laurels/Laurels.jsx
+++ restaurant/src/container/Laurels/Laurels.jsx
@@ -14,9 +14,21 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
   </div>
 );
 
+
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Awards & recognition" />
+      <h1 className="headtext__cormorant">Our Laurels</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.laurels} alt="laurels_img" />
+    </div>
   </div>
 );
 

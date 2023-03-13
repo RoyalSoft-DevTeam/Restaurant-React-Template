30d2119c8e4a03dd9126f5848b818b933efb3735
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:56:05 +0300

    started on awards section

--- restaurant/src/container/Header/Header.jsx
+++ restaurant/src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Satisfy Your Cravings" />
       <h1 className="app__header-h1">Unleash Your Senses</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Step into the lively atmosphere of AM PM and experience the ultimate dining and nightlife destination. Our vibrant spot boasts an array of delectable dishes, handcrafted cocktails, and live entertainment that will keep you coming back for more. 
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>Step into the lively atmosphere of AM | PM and experience the ultimate dining and nightlife destination. Our vibrant spot boasts an array of delectable dishes, handcrafted cocktails, and live entertainment that will keep you coming back for more. 
       <span className="p__opensans wrap" style={{ margin: '0.6rem 0', display: 'flex', color: '#FDD36A', fontWeight: '600' }}>Free Goat meat! 🍖. &nbsp; Every friday and saturday from 6pm to 9pm </span>
       </p>
       <button type="button" className="custom__button">Explore Menu</button>
--- restaurant/src/container/Laurels/Laurels.jsx
+++ restaurant/src/container/Laurels/Laurels.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (

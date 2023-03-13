95dc3d38c02132e4c10813cd66784f110437aef5
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 01:35:28 +0300

    created the onclick functionality

--- restaurant/src/container/Gallery/Gallery.jsx
+++ restaurant/src/container/Gallery/Gallery.jsx
@@ -33,3 +33,14 @@ const Gallery = () => {
               <BsInstagram className="gallery__image-icon" />
             </div>
           ))}
+          </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;

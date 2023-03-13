5460b9e9d31fd1b2a30eafeaec0be6a408cfb34f
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:57:20 +0300

    created the card for award

--- restaurant/src/container/Laurels/Laurels.jsx
+++ restaurant/src/container/Laurels/Laurels.jsx
@@ -4,6 +4,16 @@ import { SubHeading } from '../../components';
 import { images, data } from '../../constants';
 import './Laurels.css';
 
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: '#DCCA87' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
 const Laurels = () => (
   <div>
     Laurels

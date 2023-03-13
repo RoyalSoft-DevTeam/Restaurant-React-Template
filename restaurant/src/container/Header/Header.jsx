a5423bc64746b130e3c75c34526402b4b8a7ff2c
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-13 18:29:07 +0300

    created a banner image

--- restaurant/src/container/Header/Header.jsx
+++ restaurant/src/container/Header/Header.jsx
@@ -7,10 +7,10 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
+      <SubHeading title="Satisfy Your Cravings" />
       <h1 className="app__header-h1">Unleash Your Senses</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Step into the lively atmosphere of AM PM and experience the ultimate dining and nightlife destination. Our vibrant spot boasts an array of delectable dishes, handcrafted cocktails, and live entertainment that will keep you coming back for more. 
-      <span className="p__opensans wrap" style={{ margin: '0.6rem 0', display: 'flex', color: '#FDD36A', fontWeight: '600' }}>Free Goat meat.  Every friday and saturday from 6pm to 9pm </span>
+      <span className="p__opensans wrap" style={{ margin: '0.6rem 0', display: 'flex', color: '#FDD36A', fontWeight: '600' }}>Free Goat meat 🍖! &nbsp; Every friday and saturday from 6pm to 9pm </span>
       </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>

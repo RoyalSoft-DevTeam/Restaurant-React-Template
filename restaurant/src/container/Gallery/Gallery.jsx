34fc9e1559d9c615c2c4700962ead6b000542831
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 01:33:29 +0300

    wrote a subheading for the gallery

--- restaurant/src/container/Gallery/Gallery.jsx
+++ restaurant/src/container/Gallery/Gallery.jsx
@@ -21,3 +21,7 @@ const Gallery = () => {
     <div className="app__gallery flex__center">
       <div className="app__gallery-content">
         <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type="button" className="custom__button">View More</button>
+      </div>

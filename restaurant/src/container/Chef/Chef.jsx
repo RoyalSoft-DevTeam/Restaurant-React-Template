3ff09254b984fd3de84d99a3da38f8bff088b4ec
SmileDev10162 <SmileDev10162@gmail.com>
2023-03-14 00:20:41 +0300

    finished on the jsx component

--- restaurant/src/container/Chef/Chef.jsx
+++ restaurant/src/container/Chef/Chef.jsx
@@ -20,3 +20,14 @@ const Chef = () => (
         </div>
         <p className="p__opensans"> auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc. </p>
       </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_image" />
+      </div>
+    </div>
+  </div>
+);
+
+export default Chef;

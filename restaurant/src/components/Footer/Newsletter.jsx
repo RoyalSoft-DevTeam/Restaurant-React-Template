48b5d21ed19d61c8daeb8bb107acdbf9249e5a7b
SmileDev10162 <SmileDev10162@gmail.com>
2023-06-22 21:04:00 +0300

    changing errror logs

--- restaurant/src/components/Footer/Newsletter.jsx
+++ restaurant/src/components/Footer/Newsletter.jsx
@@ -8,8 +8,6 @@ const Newsletter = () => {
   const [email, setEmail] = useState('');
   const formRef = useRef();
 
-  const [loading, setLoading] = useState(false);
-  
   const handleChange = (e) => {
     const { target } = e;
     const { name, value } = target;
@@ -20,20 +18,19 @@ const Newsletter = () => {
     });
   };
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    setLoading(true);
 
-    emailjs.send('service_ekhvjlh', 'template_1kql7og',{
+    emailjs.send('service_ekhvjlh', 'template_1kql7og', {
       from_email: email,
-      to_email: "elyeestatua@gmail.com",
+      to_email: 'elyeestatua@gmail.com',
     }, 'ngs9p7jI_0GhMhbS9')
       .then((result) => {
         console.log(result.text);
         alert('Thank you for subscribing!');
         setEmail('');
-      }, (error) => {
+      })
+      .catch((error) => {
         console.log(error.text);
         alert('Oops, something went wrong. Please try again later.');
       });
@@ -44,11 +41,16 @@ const Newsletter = () => {
       <div className="app__newsletter-heading">
         <SubHeading title="Newsletter" />
         <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
-        <p className="p__opensans">And never miss latest Updates!</p>
+        <p className="p__opensans">And never miss the latest updates!</p>
       </div>
       <div className="app__newsletter-input flex__center">
         <form ref={formRef} onSubmit={handleSubmit}>
-          <input type="email" placeholder="Enter your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <button type="submit" className="custom__button">Subscribe</button>
         </form>
       </div>
